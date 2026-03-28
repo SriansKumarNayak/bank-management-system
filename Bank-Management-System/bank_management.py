b329db62649d8d8f89ed6588d83de1cd798984e2
Srians Kumar Nayak <sriansnayak07@gmail.com>
2026-03-28 15:21:19 +0530

    Add JSON file saving to bank system
    Enhanced bank management system by adding JSON file storage for persistent data. Accounts are now saved and loaded automatically, making the system more realistic

--- Bank-Management-System/bank_management.py
+++ Bank-Management-System/bank_management.py
@@ -1,19 +1,55 @@
+import json
+
 class Account:
 
-    account_number = 1001
+    account_no_counter = 1001
 
     def __init__(self, holder_name, balance, pin):
-        self.account_number = Account.account_number
+        self.account_no = Account.account_no_counter
         self.holder_name = holder_name
         self.balance = balance
         self.pin = pin
 
-        Account.account_number += 1
+        Account.account_no_counter += 1
 
     def display(self):
-        print(f"Account No: {self.account_number}\nName: {self.holder_name}\nBalance: ₹{self.balance}")
+        print(f"Account No: {self.account_no}\nName: {self.holder_name}\nBalance: ₹{self.balance}")
+
+def save_accounts():
+    data = []
+
+    for acc in accounts:
+        data.append({
+            "account_no" : acc.account_no,
+            "holder_name" : acc.holder_name,
+            "balance" : acc.balance,
+            "pin" : acc.pin
+        })
+        
+    with open('accounts.json', 'w') as file:
+        json.dump(data, file, indent = 4)
+
+def load_accounts():
+    try:
+        with open('accounts.json', 'r') as file:
+            data = json.load(file)
+
+            for item in data:
+                acc = Account(
+                    item['holder_name'],
+                    item['balance'],
+                    item['pin']
+                )
+                acc.account_no = item['account_no']
+                accounts.append(acc)
+
+            if accounts:
+                Account.account_no_counter = accounts[-1].account_no + 1
+    except (FileNotFoundError, json.JSONDecodeError):
+        pass
 
 accounts = []
+load_accounts()
 
 while True:
     print("\n===== Bank Management System =====\n")
@@ -26,171 +62,193 @@ while True:
 
     while True:
         try:
-            choice = int(input("Enter choice (1-6): "))
+            choice = int(input("Enter Your Choice (1-6): "))
             break
         except ValueError:
             print("\nChoose from (1-6) only...\n")
-
+        
     if choice == 1:
         print("\n===== Create Account =====\n")
-        
-        #Name Input
         while True:
-            name = input("Enter Account Holder Name: ").strip()
-            if name:
+            name = input("Enter Your Name (or type '0' to cancel): ").strip()
+            if name == "0":
+                break
+            elif name:
                 break
             else:
                 print("\nName cannot be empty!\n")
-        
-        #Balance Input
+        if name == "0":
+            continue
+                
+        while True:
+            pin = input("Set 4-digit PIN: ").strip()
+            if pin.isdigit() and len(pin) == 4:
+                break
+            
+            if not pin.isdigit():
+                print("\nEnter Number Only...\n")
+            
+            else:
+                print("\nPIN must be exactly 4 digits\n")
+
         while True:
             try:
-                balance = float(input("Enter Initial Balance: "))
+                balance = float(input("Enter Initial Balance: ₹"))
                 if balance < 0:
-                    print("Balance cannot be negative")
+                    print("\nBalance cannot be negative!\n")
                     continue
                 break
             except ValueError:
                 print("\nInvalid amount! Enter a number.\n")
 
-        #PIN set-up
-        while True:
-            pin = input("Set 4-digit PIN: ").strip()
-            if pin.isdigit() and len(pin) == 4:
-                break
-            else:
-                print("\nPIN must be exactly 4 digits\n")
-                
-        #Account Created
         accounts.append(Account(name, balance, pin))
-        print("\n----------------------------------------")
-        print("Account Created Successfully!")
-        print(f"Account No: {accounts[-1].account_number}")
+        save_accounts()
+
+        print("\n-------------------------------")
+        print("Account Created Successfully!\n")
+        print(f"Account No: {accounts[-1].account_no}")
         print(f"Name: {accounts[-1].holder_name}")
-        print("----------------------------------------")
+        print(f"Current Balance: ₹{accounts[-1].balance}")
+        print("-------------------------------\n")
 
     elif choice == 2:
+        print("\n===== Account Records =====\n")
         if not accounts:
             print("No Record Found!")
         else:
-            print("\n--- Account Records ---")
-            print("----------------------------------------")
+            print("-------------------------------")
             for account in accounts:
                 account.display()
-                print("----------------------------------------")
+                print("-------------------------------")
 
     elif choice == 3:
         print("\n===== Deposit Money =====\n")
+
         try:
-            account_no = int(input("Enter Your Account Number: "))
+            acc_no = int(input("Enter Account No (or type '0' to cancel): "))
         except ValueError:
-            print("Invalid Account Number!")
+            print("\nInvalid Account Number!\n")
+            continue
+
+        if acc_no == 0:
             continue
 
         found = False
         for account in accounts:
-            if account.account_number == account_no:
+            if account.account_no == acc_no:
                 found = True
-                pin = input("Enter PIN: ").strip()
-                if pin != account.pin:
-                    print("\nIncorrect PIN!\n")  
-                    break
-
+                
                 while True:
                     try:
                         amount = float(input("Enter amount to deposit: ₹"))
                         if amount <= 0:
-                            print("Amount must be greater than 0!")
+                            print("\nAmount must be greater than 0!\n")
                             continue
                         break
                     except ValueError:
-                        print("\nInvalid amount\n")
+                        print("\nInvalid amount\n") 
+                
 
-                account.balance += amount
-                print(f"\n₹{amount} Deposited Successfully")
-                print(f"Current Balance: ₹{account.balance}")
+                pin = input("Enter PIN to confirm: ").strip()
+                if pin == account.pin:
+                    account.balance += amount
+                    save_accounts() 
+                    print(f"\n₹{amount} Deposited Successfully")
+                    print(f"Current Balance: ₹{account.balance}")  
+                else:
+                    print("\nIncorrect PIN! Transaction Cancelled.\n")
+
                 break
-
+    
         if not found:
             print("\nAccount Not Found!\n")
 
     elif choice == 4:
         print("\n===== Withdraw Money =====\n")
-        try:
-            account_no = int(input("Enter Your Account Number: "))
-        except ValueError:
-            print("Invalid Account Number!")
-            continue
     
-        found = False
-        for account in accounts:
-            if account.account_number == account_no:
-                found = True
-                pin = input("Enter PIN: ").strip()
-                if pin != account.pin:
-                    print("\nIncorrect PIN!\n")
-                    break
-                while True:
-                    try:
-                        amount = float(input("Enter amount to withdraw: ₹"))
-                        if amount <= 0:
-                            print("Amount must be greater than 0!")
-                            continue
-                        if amount > account.balance:
-                            print("\nInsufficient Balance!\n")
-                            continue
-                        break
-                    except ValueError:
-                        print("\nInvalid amount!\n")
-
-                account.balance -= amount
-                print(f"\n₹{amount} Withdrawn Successfully!")
-                print(f"Remaining Balance: ₹{account.balance}")
-                break
-        if not found:
-             print("\nAccount Not Found!\n")
-
-    elif choice == 5:
-        print("\n===== Transfer Money =====\n")
-
         try:
-            sender_acc_no = int(input("Enter Your Account Number: "))
-        except ValueError:
-            print("Invalid Account Number!")
-            continue
-
-        sender = None
-        for account in accounts:
-            if account.account_number == sender_acc_no:
-                sender = account
-                break
-        
-        if not sender:
-            print("Sender account not found!")
-            continue
-
-        pin = input("Enter PIN: ").strip()
-        if pin != sender.pin:
-            print("\nIncorrect PIN!\n")
-            continue
-
-        try:
-            receiver_acc_no = int(input("Enter Receiver Account Number: "))
+            acc_no = int(input("Enter Account No (or type '0' to cancel): "))
         except ValueError:
             print("\nInvalid Account Number!\n")
             continue
 
+        if acc_no == 0:
+            continue
+
+        found = False
+        for account in accounts:
+            if account.account_no == acc_no:
+                found = True
+                
+                while True:
+                    try:
+                        amount = float(input("Enter amount to withdraw: ₹"))
+                        if amount > account.balance:
+                            print("\nInsufficient Balance!\n")
+                            continue
+                        elif amount <= 0:
+                            print("\nAmount must be greater than 0!\n")
+                            continue
+                        break
+                    except ValueError:
+                        print("\nInvalid amount\n") 
+                
+
+                pin = input("Enter PIN to confirm: ").strip()
+                if pin == account.pin:
+                    account.balance -= amount
+                    save_accounts() 
+                    print(f"\n₹{amount} Withdrawn Successfully")
+                    print(f"Current Balance: ₹{account.balance}")  
+                else:
+                    print("\nIncorrect PIN! Transaction Cancelled.\n")
+
+                break
+    
+        if not found:
+            print("\nAccount Not Found!\n")
+
+    elif choice == 5:
+        print("\n===== Transfer Money =====\n")
+        while True:
+            try:
+                sender_acc = int(input("Enter Your Account No (or type '0' to cancel): "))
+                break
+            except ValueError:
+                print("Invalid Account Number!")
+                continue
+        if sender_acc == 0:
+            continue
+
+        sender = None
+        for account in accounts:
+            if account.account_no == sender_acc:
+                sender = account
+                break
+
+        if not sender:
+            print("\nSender Account Not Found!\n")
+            continue
+
+        while True:
+            try:
+                receiver_acc = int(input("Enter Receiver Account No: "))
+                break
+            except ValueError:
+                print("Invalid Account Number!")
+                continue
+
         receiver = None
         for account in accounts:
-            if account.account_number == receiver_acc_no:
+            if account.account_no == receiver_acc:
                 receiver = account
                 break
 
         if not receiver:
-            print("\nReceiver account not found!\n")
+            print("\nReceiver Account Not Found!\n")
             continue
 
-        if sender.account_number == receiver.account_number:
+        if sender.account_no == receiver.account_no:
             print("\nCannot transfer to same account!\n")
             continue
 
@@ -200,22 +258,25 @@ while True:
                 if amount <= 0:
                     print("\nAmount must be greater than 0!\n")
                     continue
-                if amount > sender.balance:
-                    print("Insufficient balance")
+                elif amount > sender.balance:
+                    print("\nInsufficient balance!\n")
                     continue
                 break
             except ValueError:
                 print("\nInvalid amount!\n")
 
-        sender.balance -= amount
-        receiver.balance += amount
-
-        print(f"\n₹{amount} transferred from Acc_no: {sender.account_number} to Acc_no: {receiver.account_number} Successfully")
-        print(f"Your Current Balance: ₹{sender.balance}")
+        pin = input("Enter Your PIN to confirm: ").strip()
+        if pin == sender.pin:
+            sender.balance -= amount
+            receiver.balance += amount
+            save_accounts()
+            print(f"\n₹{amount} transferred from Acc_no: {sender.account_no} to Acc_no: {receiver.account_no} Successfully")
+            print(f"Your Current Balance: ₹{sender.balance}")
+        else:
+            print("\nIncorrect PIN! Transaction Cancelled.\n")
 
     elif choice == 6:
-        print("Thank You... Bye!")
+        print("\nThank You... Bye!")
         break
     else:
         print("\nChoose from (1-6) only...\n")
-
